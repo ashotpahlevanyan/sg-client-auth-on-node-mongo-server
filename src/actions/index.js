d76182748c3b4455f4915f6ac0a20bd5a66b52fc
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2020-11-24 11:45:30 +0400

    added sign in

--- src/actions/index.js
+++ src/actions/index.js
@@ -20,6 +20,26 @@ export const signup = (formProps, callback) => async dispatch => {
   }
 };
 
+// usual action creator structure will be this
+export const signin = (formProps, callback) => async dispatch => {
+  try {
+    const response = await axios.post('http://localhost:3090/signin', formProps);
+
+    dispatch({
+      type: AUTH_USER,
+      payload: response.data.token
+    });
+
+    // set the token to local storage
+    localStorage.setItem('token', response.data.token);
+
+    callback();
+  } catch (e) {
+    dispatch({ type: AUTH_ERROR, payload: 'Invalid login credentials' });
+  }
+};
+
+
 export const signout = () => {
   localStorage.removeItem('token');
 
--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import App from './components/App';
 import Welcome from './components/Welcome';
 import Signup from './components/auth/Signup';
 import Signout from './components/auth/Signout';
+import Signin from './components/auth/Signin';
 import Feature from './components/Feature';
 import reducers from './reducers';
 
@@ -29,6 +30,7 @@ ReactDOM.render(
         <Route path="/signup" component={Signup} />
         <Route path="/feature" component={Feature} />
         <Route path="/signout" component={Signout} />
+        <Route path="/signin" component={Signin} />
       </App>
     </BrowserRouter>
   </Provider>,
